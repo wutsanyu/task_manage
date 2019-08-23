e4161db23bd9ed53b757d26773db4b62aeaed524
wutsanyu <b97101053@gmail.com>
2019-08-23 18:04:38 +0800

    migrate TaskList

---
+++ db/migrate/20190823093625_create_task_lists.rb
@@ -0,0 +1,10 @@
+class CreateTaskLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :task_lists do |t|
+      t.string :title
+      t.string :class
+
+      t.timestamps
+    end
+  end
+end
